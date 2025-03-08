928c7ab79d03487850225fa75fbfd680233def31
junaid014 <shaik.junaids.j.m@gmail.com>
2025-03-08 09:19:13 +0530

    select and match test completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,16 @@ import img from "../asserts/image.png"
 function App() {
   const [users, setUsers] = useState([])
   let [curr, setCurr] = useState(false);
-  let [mat,setMat]=useState([]);
+  let [mat,setMat]=useState(-1);
+
+  let  [add,setAdd]=useState(false);
+  let  [del,setDel]=useState(false);
+  let [upd,setUpd]=useState(false);
 
   let allDetails = async () => {
-    let get = await axios.get("http://localhost:8080/getAll");
+    
     try {
+      let get = await axios.get("http://localhost:8080/getAll");
       if (get.data.statusCode == 200) {
         setUsers(get.data.data);
       } else console.log("error occured");
@@ -18,16 +23,11 @@ function App() {
     }
 
   }
-  useEffect(() => {
-    allDetails();
-  }, []);
-
-
+  
 
   let match = async (i) => {
-
-    let get = await axios.get(`http://localhost:8080/match/${i}`)
     try {
+      let get = await axios.get(`http://localhost:8080/match/${i}`)
       if (get.data.statusCode == 200) {
         setUsers(get.data.data);
       } else console.log("error occured");
@@ -37,34 +37,50 @@ function App() {
     }
   }
 
-  let handle=(i)=>{
-    setCurr((prev) => !prev);
-    match(i);
 
+  let getMatch=(i)=>{
+    setCurr(true);
+    setMat(i);
+    
   }
+
+  useEffect(() => {
+    allDetails();
+  },[curr]);
+
+  useEffect(() => {
+    if (curr && mat !== -1) {
+      match(mat);
+    }
+  }, [curr, mat]);
+  
+
   return (
 
 
     <>
-      <img src={img} alt="image" style={{ width: "100%", height: "30%", backgroundSize: "auto" }} />
-      <div className='flex items-center pt-10 gap-10 justify-center flex-col w-[100%]'>
+      <img src={img} alt="image" style={{ width: "100%", height: "30vh", backgroundSize: "auto" }} />
+      <div className='flex items-center pt-5 gap-10 justify-center flex-col w-[100%]'>
         <div>
           <h1> Welcome to Dating Suggestions</h1>
         </div>
 
+        <div>
+          <button className="border px-4 py-1 rounded bg-blue-400 " style={{ display: !curr ? "none" : "block" } } onClick={()=>setCurr(false)}>Back</button>
+        </div>
 
-        <div className="p-4 rounded-full text-center">
 
+        <div className="p-1 rounded-full text-center">
 
-          <table className="border-collapse w-full">
+          <table className=" rounded-2xl w-full">
             <thead>
               <tr className="bg-gray-200">
                 <th className="border px-10 py-2">Name</th>
-                <th className="border px-10 ">Gender</th>
-                <th className="border px-10 ">Age</th>
-                <th className="border px-10 ">Interest</th>
+                <th className="border px-5 ">Gender</th>
+                <th className="border px-2 ">Age</th>
+                <th className="border px-7 ">Interest</th>
                 <th className="border px-10 ">Match</th>
-                <th className="border px-10 ">Update</th>
+                <th className="border px-10 ">Update/Delete</th>
               </tr>
             </thead>
             <tbody>
@@ -77,49 +93,22 @@ function App() {
                     {i.interest.map((int) => int.interest).join(", ")}
                   </td>
                   <td>
-                  <button className="bg-blue-500 text-white px-2 py-1 rounded" onClick={()=>handle(i.id)} style={{ visibility: curr ? "visible" : "hidden" }}> Top Match</button>
+                  <button className="bg-blue-500 text-white px-2 py-1 rounded" onClick={()=>getMatch(i.id)} style={{ display: curr ? "none" : "inline-block"}}> Top Match</button>
 
                   </td>
                   <td className="p-4">
-                    <button className="bg-blue-500 px-4 py-1 rounded">Edit</button>
+                    <button className="bg-blue-500 px-4 m-1 py-1 rounded">Edit</button>
+                    <button className="bg-blue-500 px-4 py-1 rounded">Delete</button>
                   </td>
                 </tr>
               ))}
 
           </tbody>
         </table>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
       </div>
-
-
-
     </div >
-
-
     </>
   )
 }
-
-
-
 
 export default App
